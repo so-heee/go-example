60c831ff380788d8ed1a20b9b9c2dd0f93477aca
so-heee <so.heee.mochi@gmail.com>
2021-12-14 11:19:10 +0900

    add section9

---
+++ udemy/section9/go.mod
@@ -0,0 +1,3 @@
+module github.com/so-heee/go-example/udemy/section9
+
+go 1.16
---
+++ udemy/section9/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	//スライス
+	var sl []int
+	fmt.Println(sl)
+
+	var sl2 []int = []int{100, 200}
+	fmt.Println(sl2)
+
+	sl3 := []string{"A", "B"}
+	fmt.Println(sl3)
+
+	sl4 := make([]int, 5)
+	fmt.Println(sl4)
+
+	sl2[0] = 1000
+	fmt.Println(sl2)
+
+	sl5 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sl5)
+	fmt.Println(sl5[0])
+	fmt.Println(sl5[2:4])
+	fmt.Println(sl5[:2])
+	fmt.Println(sl5[2:])
+	fmt.Println(sl5[:])
+	fmt.Println(sl5[len(sl5)-1])
+	fmt.Println(sl5[1 : len(sl5)-1])
+}
