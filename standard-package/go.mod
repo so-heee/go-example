d20fdaacda205a9cab832548bc4ae3e545837596
so-heee <so.heee.mochi@gmail.com>
2022-01-26 11:14:08 +0900

    update

---
+++ standard-package/go.mod
@@ -0,0 +1,3 @@
+module github.com/so-heee/go-example/standard-package
+
+go 1.17
---
+++ standard-package/os.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// パッケージosは、オペレーティングシステムの機能に対するプラットフォームに依存しないインターフェイスを提供します。
+// 主な機能としては、ファイルやディレクトリの操作、環境変数の操作、プロセス情報の参照などが含まれます。
+
+// Exit.
+func Exit() {
+	// os.Exit(1)
+	// fmt.Println("Start")
+
+	defer func() {
+		fmt.Println("defer")
+	}()
+	os.Exit(0)
+}
+
+// Fatal.
+func Fatal() {
+	_, err := os.Open("A.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Args.
+func Args() {
+	fmt.Println(os.Args[0])
+	fmt.Println(os.Args[1])
+	fmt.Println(os.Args[2])
+	fmt.Println(os.Args[3])
+
+	fmt.Printf("length=%d\n", len(os.Args))
+	for i, v := range os.Args {
+		fmt.Println(i, v)
+	}
+
+}
+
+// File.
+func File() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+}
+
+// FileCreate.
+func FileCreate() {
+	f, _ := os.Create("foo.txt")
+	f.Write([]byte("Hello\n"))
+	f.WriteAt([]byte("Golang"), 6)
+	f.Seek(0, os.SEEK_END)
+	f.WriteString("Yaah")
+}
+
+// FileRead.
+func FileRead() {
+	f, err := os.Open("foo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	bs := make([]byte, 128)
+	n, _ := f.Read(bs)
+	fmt.Println(n)
+	fmt.Println(string(bs))
+
+	bs2 := make([]byte, 128)
+
+	nn, _ := f.ReadAt(bs2, 10)
+	fmt.Println(nn)
+	fmt.Println(string(bs2))
+}
+
+// FileOpen.
+func FileOpen() {
+	f, err := os.OpenFile("foo.txt", os.O_RDONLY, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer f.Close()
+
+	bs := make([]byte, 128)
+	n, err := f.Read(bs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(n)
+	fmt.Println(string(bs))
+
+}
