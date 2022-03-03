2bda7d8f16e9cc928057c3471e3e9ca44a487ae5
so-heee <so.heee.mochi@gmail.com>
2022-03-03 10:57:58 +0900

    add variables

---
+++ learning/my-learning/variables/go.mod
@@ -0,0 +1,3 @@
+module github.com/so-heee/go-example/learning/my-learning/variables
+
+go 1.17
---
+++ learning/my-learning/variables/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// グローバル変数
+var I = 100
+
+func main() {
+
+	// 明示的な宣言
+	var i int
+	var f float64
+	fmt.Println(i)
+	fmt.Println(f)
+
+	// 明示的な宣言＋初期化
+	var i2 int = 100
+	fmt.Println(i2)
+
+	// 暗黙的な宣言
+	var i3 = 100
+	fmt.Println(i3)
+
+	// var の省略
+	i4 := 100
+	fmt.Println(i4)
+
+	// まとめて宣言
+	var i5, s = 100, "abc"
+	var (
+		i6 = 100
+		s2 = "abc"
+	)
+	i7, s3 := 100, "abc"
+	fmt.Println(i5, i6, i7, s, s2, s3)
+
+	fmt.Println(I)
+
+}
+
+/*
+
+宣言時に初期値を設定しない場合の値
+- 数値型(int,floatなど): 0
+- bool型: false
+- string型: "" (空文字列( empty string ))
+- その他: nil
+
+*/
